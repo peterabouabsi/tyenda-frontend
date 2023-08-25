b40c74c4cdd0520acea505a4e9d411978888effa
Peter Abou Absi <peteraba60@gmail.com>
2023-08-25 17:24:15 +0300

    Update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: false,
-  authenticatedBaseUrl: "https://127.0.0.1:5001/api/Authentication",
-  anonymousBaseUrl: "https://127.0.0.1:5001/api/Anonymous",
-  fileBaseUrl: "https://127.0.0.1:5001/files/",
+  authenticatedBaseUrl: "https://localhost:5001/api/Authentication",
+  anonymousBaseUrl: "https://localhost:5001/api/Anonymous",
+  fileBaseUrl: "https://localhost:5001/files/",
   mapTile: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
   oneSignal: {
-    appId: '87a481ac-5cb2-43d0-839c-3d9775159856'
+    appId: 'f96b8dc9-d261-4b3b-b48c-9ac4c7ff9ef9'
   },
   googleOAuthClientId: '677712129576-t9rhnfjj52t86hhs6ib4rl610ebdlu7j.apps.googleusercontent.com',
 
